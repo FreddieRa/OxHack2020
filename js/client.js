e4b6db17e6026a495b5c044416c9543c92e2f29b
MatthewMunks <matthewmunks@gmail.com>
2020-05-30 16:29:27 +0100

    Commit 2 Still trying to get classes to work

--- js/client.js
+++ js/client.js
@@ -1,6 +1,20 @@
 $(function() {
 var socket = io();    //Gets the socket from the server (?)
 
+class GetGiph{
+  constructor(APIkey = "ityag1M5myXGHtCjPeqtXtBYa38EUo46"){
+      this.GphApiClient = require('giphy-js-sdk-core');
+      this.client = GphApiClient(APIkey);
+  }
+
+  getKey() {
+    alert("iN")
+      $('#messages').append($('<li>').text("this.client.APIkey"));
+      $('#messages').append($('<li>').text(this.client.APIkey));
+      alert(this.client.APIkey);
+  }
+}
+
 $('form').submit(function(){
   socket.emit('chat message', $('#m').val()); //Sending a message to server
   $('#m').val('');  //Setter
@@ -8,7 +22,9 @@ $('form').submit(function(){
 });
 
 socket.on('chat message', function(msg){    //Recieving from server
-  $('#messages').append($('<li>').text(msg));   //Add message to #messages
+  $('#messages').append($('<li>').text(msg + " ah"));   //Add message to #messages
+  var GiphSearch = new GetGiph();
+  GiphSearch.getKey();
   window.scrollTo(0, document.body.scrollHeight);
 });
 });
