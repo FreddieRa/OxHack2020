67b36d46389da85281836e609ae926a01b8a05de
Freddie Rawlins <freddie.rawlins@mail.com>
2020-05-30 14:10:57 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
-var socket = io();
+var socket = io();    //Gets the socket from the server (?)
 
 $('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
+  socket.emit('chat message', $('#m').val()); //Sending a message to server
+  $('#m').val('');  //Setter
   return false;
 });
 
-socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
-    window.scrollTo(0, document.body.scrollHeight);
+socket.on('chat message', function(msg){    //Recieving from server
+  $('#messages').append($('<li>').text(msg));   //Add message to #messages
+  window.scrollTo(0, document.body.scrollHeight);
 });
