d6f4987addd3312211707ad3cf8ed2a877eae412
MatthewMunks <matthewmunks@gmail.com>
2020-05-31 10:38:40 +0100

    4 tiles commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var giphy = require('giphy-js-sdk-core');
+//var giphy = require('giphy-js-sdk-core');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
